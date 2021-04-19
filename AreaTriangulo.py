a7bae046be84a42eddf795ea38be797404e8cdd0
Frank7729 <>
2021-04-19 15:20:22 +0000

    Ejemplos en java, python y go -correcion ignore

---
+++ AreaTriangulo.py
@@ -0,0 +1,9 @@
+#Definir variables y otros
+print("Ejercicio 01: Area Triangulo")
+#Datos de entrada
+b=int(input("Ingrese Base:"))
+h=int(input("Ingrese Altura:"))
+#Proceso
+area=(b*h)/2
+#Datos de salida
+print("El area es: ", area)
